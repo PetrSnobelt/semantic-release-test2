115e76e5598d420ac2093a80f3ee2ee5e0de7a46
Petr Snobelt <petr.snobelt@kiwi.com>
2018-08-16 14:05:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("hello world next time, with better commit message. Try fix it now")
+console.log("Release?!")
+console.log("one wild ssh with valid token?!")
+console.log("Another test")
